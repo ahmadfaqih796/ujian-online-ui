e270837cf835c8807781b77185261bbde4e7af6a
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2022-09-02 13:19:20 +0700

    pelajaran

--- src/pages/admin/konfigurasi/pelajaran.js
+++ src/pages/admin/konfigurasi/pelajaran.js
@@ -72,7 +72,7 @@ const Pelajaran = ({ lesson }) => {
             variant="contained"
             onClick={() => handleOpenModal("add")}
           >
-            Tambahkan
+            Tambahkanaa
           </Button>
         </Box>
         <BaseTable tableHead={LESSON_CELLS} data={lesson}>
