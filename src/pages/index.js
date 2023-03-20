2e255f2506a603577cf2eee9b919f2379eda9184
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-03-20 08:49:02 +0700

    login

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,10 @@ export default function Home() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
