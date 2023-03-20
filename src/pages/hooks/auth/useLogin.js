83bc8a997e9978c4db7f9ea44a72d94a7c2b6b6d
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-03-20 08:53:50 +0700

    hook login

---
+++ src/pages/hooks/auth/useLogin.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Axios from "axios";
+
+const useLogin = () => {
+  const router = useRouter();
+  const [state, setState] = useState({
+    loading: false,
+  });
+
+  const handleLoading = (status) => {
+    setState((prevState) => ({
+      ...prevState,
+      loading: status,
+    }));
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    handleLoading(true);
+
+    const { target } = event;
+    const { email, password } = target;
+
+    const data = {
+      email: email.value,
+      password: password.value,
+      strategy: "local",
+    };
+
+    try {
+      const { data: res } = await Axios.post("/api/login", data);
+
+      if (res.level.level === 1) {
+        return router.replace("/apps/absent");
+      }
+      router.replace("/dashboards/dashboard");
+    } catch (error) {
+      handleLoading(false);
+      alert(error?.response?.data.message);
+      return;
+    }
+  };
+
+  return {
+    loading: state.loading,
+    handleLogin,
+  };
+};
+
+export default useLogin;
