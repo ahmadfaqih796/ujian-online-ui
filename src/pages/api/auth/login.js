5e073358397ca0733d746cbbd4207b1d88651361
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-03-27 11:56:30 +0700

    login api

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -4,9 +4,7 @@ import { withSessionRoute } from "@/lib/sessions/withSession";
 async function loginRoute(req, res) {
   try {
     const response = await loginService(req.body);
-
     const { nik } = response.user;
-
     // if (role_id != "admin") {
     //   if (job_level.level === 1 || job_level.name === "STAFF") {
     //     return res.redirect("/404").end();
@@ -17,9 +15,6 @@ async function loginRoute(req, res) {
       id: response.user.id_user,
       nik: nik,
       exp: response.authentication.payload.exp,
-      // role: response.user.role_id,
-      // level: job_level.name,
-      // company_id: response.user.company_id,
       token: response.accessToken,
     };
 
@@ -33,7 +28,6 @@ async function loginRoute(req, res) {
   } catch (error) {
     console.log(error);
     const e = error.toString();
-
     // handle error from api with response api
     if (error?.response) {
       return res.status(500).json(error.response.data);
