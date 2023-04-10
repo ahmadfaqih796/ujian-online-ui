e121f6d0271a89bb6d79625f8385ebee0347b465
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-04-10 09:31:19 +0700

    pagination services

---
+++ src/lib/services/pagination/pagination.js
@@ -0,0 +1,20 @@
+import { clone } from "ramda";
+import ServiceAdapter from "..";
+
+const pagination = async (url, context, headers = {}) => {
+  const newQuery = clone(context);
+  const perPage = newQuery?.per_page ?? 10;
+  const page = newQuery?.page ?? 1;
+
+  newQuery.$skip = page * perPage - perPage;
+  delete newQuery.page;
+  newQuery.$limit = perPage;
+  delete newQuery.per_page;
+  const data = await ServiceAdapter().get(url, {
+    params: newQuery,
+    headers: headers,
+  });
+  return data.data;
+};
+
+export default pagination;
--- src/pages/admin/users/siswa.js
+++ src/pages/admin/users/siswa.js
@@ -1,7 +1,31 @@
+import pagination from "@/lib/services/pagination/pagination";
+import WithAuth from "@/lib/sessions/withAuth";
 import React from "react";
 
-const Siswa = () => {
-  return <div>Siswa</div>;
+export const getServerSideProps = WithAuth(async ({ query, req }) => {
+  const departements = await pagination(
+    "/users",
+    {
+      ...query,
+    },
+    {
+      Authorization: req.session.user.token,
+    }
+  );
+  return {
+    props: {
+      departements,
+    },
+  };
+});
+
+const Siswa = ({ departements }) => {
+  return (
+    <>
+      {JSON.stringify(departements)}
+      siswa
+    </>
+  );
 };
 Siswa.layout = "Admin";
 export default Siswa;
