d8bd1657dd11ed7ebe03736bb595a914e15e565d
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-04-10 09:15:32 +0700

    validate response 401

--- src/lib/services/index.js
+++ src/lib/services/index.js
@@ -3,10 +3,30 @@ import axios from "axios";
 const BASE_API = process.env.NEXT_PUBLIC_BASE_API_LOCAL;
 
 function ServiceAdapter() {
-  return axios.create({
+  // return axios.create({
+  //   baseURL: BASE_API,
+  //   responseType: "json",
+  // });
+
+  const instance = axios.create({
     baseURL: BASE_API,
     responseType: "json",
   });
+
+  //validate response
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response.status === 401) {
+        return (window.location.href = "/authentication/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
 }
 
 export default ServiceAdapter;
