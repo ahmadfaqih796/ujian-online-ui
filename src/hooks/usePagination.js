c4a8e3490053be6c60b3199262e6e593b6b28d5d
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-04-10 10:05:01 +0700

    usePagination

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,37 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const usePagination = () => {
+  const router = useRouter();
+  const currentPage = router.query?.page ?? 1;
+  const currentRowPerPage = router.query?.per_page ?? 10;
+  const [page, setPage] = useState(currentPage - 1);
+  const [rowsPerPage, setRowsPerPage] = useState(currentRowPerPage);
+
+  const handleChangePage = async (event, newPage) => {
+    await router.replace({
+      pathname: router.pathname,
+      query: {
+        ...router.query,
+        page: newPage + 1,
+      },
+    });
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = async (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+    await router.replace({
+      pathname: router.pathname,
+      query: {
+        ...router.query,
+        per_page: event.target.value,
+        page: 1,
+      },
+    });
+  };
+  return { page, rowsPerPage, handleChangePage, handleChangeRowsPerPage };
+};
+
+export default usePagination;
--- src/pages/admin/users/siswa.js
+++ src/pages/admin/users/siswa.js
@@ -1,9 +1,22 @@
+import {
+  Button,
+  CardContent,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import pagination from "@/lib/services/pagination/pagination";
 import WithAuth from "@/lib/sessions/withAuth";
 import React from "react";
+import usePagination from "@/hooks/usePagination";
 
 export const getServerSideProps = WithAuth(async ({ query, req }) => {
-  const departements = await pagination(
+  const users = await pagination(
     "/users",
     {
       ...query,
@@ -14,16 +27,166 @@ export const getServerSideProps = WithAuth(async ({ query, req }) => {
   );
   return {
     props: {
-      departements,
+      users,
     },
   };
 });
 
-const Siswa = ({ departements }) => {
+const Siswa = ({ users }) => {
+  const { page, rowsPerPage, handleChangePage, handleChangeRowsPerPage } =
+    usePagination();
+  const pageContent = users.data.slice(
+    (page - 1) * rowsPerPage,
+    page * rowsPerPage
+  );
   return (
     <>
-      {JSON.stringify(departements)}
-      siswa
+      <Box
+        sx={{
+          overflow: "auto",
+          sm: "unset",
+        }}
+      >
+        <Table
+          aria-label="simple table"
+          sx={{
+            whiteSpace: "nowrap",
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography align="center" variant="h5">
+                  No
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography align="center" variant="h5">
+                  Nama
+                </Typography>
+              </TableCell>
+
+              <TableCell>
+                <Typography align="center" variant="h5">
+                  NIK
+                </Typography>
+              </TableCell>
+
+              <TableCell>
+                <Typography align="center" variant="h5">
+                  Tanggal Di Buat
+                </Typography>
+              </TableCell>
+
+              <TableCell>
+                <Typography align="center" variant="h5">
+                  Tanggal Di Update
+                </Typography>
+              </TableCell>
+
+              <TableCell>
+                <Typography align="center" variant="h5">
+                  Action
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {users.data.map((data, index) => (
+              <TableRow key={index}>
+                <TableCell>
+                  <Typography
+                    variant="h6"
+                    fontWeight="600"
+                    textAlign="center"
+                    color="textSecondary"
+                  >
+                    {index + 1 + ((page - 1) * rowsPerPage, page * rowsPerPage)}
+                  </Typography>
+                </TableCell>
+
+                {/* data name */}
+                <TableCell>
+                  <Typography
+                    variant="h6"
+                    fontWeight="600"
+                    textAlign="center"
+                    color="textSecondary"
+                  >
+                    {data.name}
+                  </Typography>
+                </TableCell>
+
+                <TableCell>
+                  <Typography
+                    variant="h6"
+                    fontWeight="600"
+                    textAlign="center"
+                    color="textSecondary"
+                  >
+                    {data.nik}
+                  </Typography>
+                </TableCell>
+
+                {/* data created at */}
+                <TableCell>
+                  <Typography
+                    variant="h6"
+                    fontWeight="600"
+                    textAlign="center"
+                    color="textSecondary"
+                  >
+                    {data.createdAt
+                      ? moment(data.createdAt).format("DD MMM YYYY, HH:mm:ss")
+                      : "-"}
+                  </Typography>
+                </TableCell>
+
+                {/* data update_at */}
+                <TableCell>
+                  <Typography
+                    variant="h6"
+                    fontWeight="600"
+                    textAlign="center"
+                    color="textSecondary"
+                  >
+                    {data.updatedAt
+                      ? moment(data.updatedAt).format("DD MMM YYYY, HH:mm:ss")
+                      : "-"}
+                  </Typography>
+                </TableCell>
+
+                <TableCell align="center">
+                  {/* <ThreeDotsMenu
+                    data={data}
+                    token={users}
+                    onClickEdit={() => usersEdit(data)}
+                    onClickDelete={() => usersDelete(data)}
+                  /> */}
+                  sss
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={users.total}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          labelDisplayedRows={({ from, to, count }) => {
+            return `Menampilkan ${from}-${to} dari ${
+              count != -1 ? count : `more than ${to}`
+            } data`;
+          }}
+          showFirstButton
+          showLastButton
+        />
+      </Box>
     </>
   );
 };
