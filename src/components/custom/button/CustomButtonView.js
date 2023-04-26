a65121325be7e57b86b7477d2c7400954f711dfe
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-04-26 11:07:08 +0700

    custom button view

--- src/components/custom/button/CustomButtonView.js
+++ src/components/custom/button/CustomButtonView.js
@@ -4,7 +4,7 @@ import { styled } from "@mui/material/styles";
 
 export const CustomButtonView = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(blue[500]),
-  backgroundColor: blue[500],
+  backgroundColor: blue[300],
   "&:hover": {
     backgroundColor: blue[700],
   },
