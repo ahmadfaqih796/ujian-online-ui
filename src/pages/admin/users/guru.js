eb956ca77029f5202fb65e589c28240f4cdd2e5b
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-04-26 10:08:05 +0700

    guru

--- src/pages/admin/users/guru.js
+++ src/pages/admin/users/guru.js
@@ -1,7 +1,129 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import pagination from "@/lib/services/pagination/pagination";
+import WithAuth from "@/lib/sessions/withAuth";
 import React from "react";
+import moment from "moment";
+import usePagination from "@/hooks/usePagination";
+import useHandleModal from "@/hooks/useHandleModal";
+import AddUserModal from "@/components/modal/user/AddUserModal";
+import DeleteModal from "@/components/modal/DeleteModal";
+import ThreeDotsMenu from "@/components/menu-items/ThreeDotsMenu";
+import DetailUserModal from "@/components/modal/user/DetailUserModal";
+import Grid from "@mui/material/Grid";
 
-const Guru = () => {
-  return <div>Guru</div>;
+export const getServerSideProps = WithAuth(async ({ query, req }) => {
+  const users = await pagination(
+    "/users",
+    {
+      ...query,
+    },
+    {
+      Authorization: req.session.user.token,
+    }
+  );
+  return {
+    props: {
+      users,
+    },
+  };
+});
+
+const Guru = ({ users }) => {
+  const [userData, setUserData] = React.useState({});
+  const { openModal, modalType, handleCloseModal, handleOpenModal } =
+    useHandleModal(false);
+  const { page, rowsPerPage, handleChangePage, handleChangeRowsPerPage } =
+    usePagination();
+
+  return (
+    <>
+      <AddUserModal
+        open={openModal}
+        type={modalType}
+        closeModalHandler={handleCloseModal}
+      />
+      <DetailUserModal
+        open={openModal}
+        type={modalType}
+        data={userData}
+        closeModalHandler={handleCloseModal}
+      />
+      <DeleteModal
+        open={openModal}
+        type={modalType}
+        data={userData}
+        closeModalHandler={handleCloseModal}
+      />
+      <Box display="flex">
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => handleOpenModal("add")}
+        >
+          Tambahkan
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          overflow: "auto",
+          sm: "unset",
+        }}
+      >
+        <Grid container spacing={2}>
+          {users.data.map((data, index) => (
+            <Grid key={index} item xs={3}>
+              <Card>{data.name}</Card>
+            </Grid>
+          ))}
+          {/* <Grid item xs={8}>
+            <Box>xs=8</Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box>xs=4</Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box>xs=4</Box>
+          </Grid>
+          <Grid item xs={8}>
+            <Box>xs=8</Box>
+          </Grid> */}
+          {/* {users.data.map((data, index) => (
+            <Card key={index} sx={{ width: "200px" }}>
+              aaaa
+            </Card>
+          ))} */}
+        </Grid>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={users.total}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          labelDisplayedRows={({ from, to, count }) => {
+            return `Menampilkan ${from}-${to} dari ${
+              count != -1 ? count : `more than ${to}`
+            } data`;
+          }}
+          showFirstButton
+          showLastButton
+        />
+      </Box>
+    </>
+  );
 };
 Guru.layout = "Admin";
 export default Guru;
