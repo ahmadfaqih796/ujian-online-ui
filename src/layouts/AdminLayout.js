6ff1623512284808874dbbc4f538ed741cde70e5
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-05-03 08:44:23 +0700

    background

--- src/layouts/AdminLayout.js
+++ src/layouts/AdminLayout.js
@@ -59,7 +59,6 @@ const AdminLayout = ({ children, window }) => {
       <Box
         component="main"
         sx={{
-          backgroundColor: "#e7ebf0",
           flexGrow: 1,
           p: 3,
           width: { sm: `calc(100% - ${drawerWidth}px)` },
