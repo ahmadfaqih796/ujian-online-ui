c44bbf1270df5352987bc98dc07e72f907f6c8c2
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-05-05 11:04:39 +0700

    delete api lesson

---
+++ src/pages/api/lesson/[id].js
@@ -0,0 +1,19 @@
+import { deleteLesson } from "@/lib/services/lesson";
+import { withSessionRoute } from "@/lib/sessions/withSession";
+
+async function handler(req, res) {
+  const { user: userSession } = req.session;
+
+  if (req.method === "DELETE") {
+    try {
+      const { id } = req.query;
+      const response = await deleteLesson(id, userSession.token);
+      return res.json(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ ok: false });
+    }
+  }
+}
+
+export default withSessionRoute(handler);
