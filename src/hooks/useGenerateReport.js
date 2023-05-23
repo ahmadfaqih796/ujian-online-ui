369c297d708baf343d228ca3712a473c270a69fc
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-05-23 14:25:27 +0700

    library js-file-download

---
+++ src/hooks/useGenerateReport.js
@@ -0,0 +1,29 @@
+import ServiceAdapter from "@/lib/services";
+import * as FileDownload from "js-file-download";
+import React from "react";
+const useGenerateReport = () => {
+  const [loading, setLoading] = React.useState(false);
+  const generate = async ({ data, onSuccess, onError }) => {
+    setLoading(true);
+    try {
+      const response = await ServiceAdapter().post("/export/users", data, {
+        responseType: "arraybuffer",
+      });
+      FileDownload(response.data, `guru.xlsx`);
+      setLoading(false);
+      onSuccess();
+      return;
+    } catch (error) {
+      console.log(error.response?.data?.byteLength);
+      setLoading(false);
+      const errMessage =
+        error.response?.data?.message ?? "Maaf terjadi kesalahan pada server";
+      onError(errMessage);
+      onError(errMessage);
+      return;
+    }
+  };
+  return { generate, loading };
+};
+
+export default useGenerateReport;
