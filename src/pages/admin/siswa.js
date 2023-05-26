3c2873fed4c36705224964f4e3c053dcb45b6ed2
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-05-26 14:12:39 +0700

    library xlsx

--- src/pages/admin/siswa.js
+++ src/pages/admin/siswa.js
@@ -1,22 +1,55 @@
+import { Typography } from "@mui/material";
 import React from "react";
-import readXlsxFile from "read-excel-file";
+import * as XLSX from "xlsx";
 
 const Siswa = () => {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const { target } = event;
-    const { input } = target;
-    console.log("xxxx", input.files[0]);
-    readXlsxFile(input.files[0]).then((rows) => {
-      console.log("aaaaaaaa", rows);
+  const [table, setTable] = React.useState([]);
+  console.log("qqqq", table);
+
+  const convertToJson = async (headers, data) => {
+    const rows = [];
+    console.log("data", data);
+    data.forEach(async (row) => {
+      let rowData = {};
+      row.forEach(async (element, index) => {
+        rowData[headers[index]] = element;
+      });
+      console.log("rowDatattt", rowData);
+      rows.push(rowData);
     });
+    setTable(rows);
+    return rows;
+  };
+
+  const handleSubmit = (event) => {
+    const file = event.target.files[0];
+    console.log("xxxx", file);
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const bstr = e.target.result;
+      const workBook = XLSX.read(bstr, { type: "binary" });
+      const workSheetName = workBook.SheetNames[0];
+      const workSheet = workBook.Sheets[workSheetName];
+      const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 });
+      const headers = fileData[0];
+      const heads = headers.map((head) => ({ title: head, field: head }));
+      fileData.splice(0, 1);
+      convertToJson(headers, fileData);
+    };
+    reader.readAsBinaryString(file);
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <input type="file" name="input" id="input" onChange={handleSubmit} />
+      {table.map((item, index) => (
+        <>
+          <Typography>{item?.SOAL}</Typography>
+        </>
+      ))}
+      {/* <form onSubmit={handleSubmit}>
         <input type="file" name="input" id="input" />
         <button type="submit">click</button>
-      </form>
+      </form> */}
     </>
   );
 };
