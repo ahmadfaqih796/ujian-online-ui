66d54335ea96b1c362d3e1260d278532e7664710
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-28 00:00:23 +0700

    with auth

--- src/lib/sessions/withAuth.js
+++ src/lib/sessions/withAuth.js
@@ -2,18 +2,28 @@ import { withSessionSsr } from "./withSession";
 
 const WithAuth = (gssp) =>
   withSessionSsr(async (context) => {
-    const user = context.req.session.user;
-
-    // you can check the user in your DB here
-    if (!user) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/authentication/login",
-        },
-      };
+    try {
+      const user = context.req.session.user;
+      if (!user) {
+        return {
+          redirect: {
+            permanent: false,
+            destination: "/authentication/login",
+          },
+        };
+      }
+      return await gssp(context);
+    } catch (error) {
+      console.log(error);
+      if (error.response.data.code === 401) {
+        return {
+          redirect: {
+            permanent: false,
+            destination: "/authentication/login",
+          },
+        };
+      }
     }
-
     return await gssp(context);
   });
 
