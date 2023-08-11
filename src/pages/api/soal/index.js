362c545e3abd2964f275bea2305b4d4c62616d8f
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-08-11 10:35:07 +0700

    add soal modal

--- src/pages/api/soal/index.js
+++ src/pages/api/soal/index.js
@@ -4,6 +4,19 @@ import { withSessionRoute } from "@/lib/sessions/withSession";
 async function handler(req, res) {
   const { user: userSession } = req.session;
 
+  if (req.method === "POST") {
+    try {
+      const { body } = req;
+      const response = await addLesson(body, userSession.token);
+      return res.json({
+        message: "berhasil menambahkan soal",
+        response,
+      });
+    } catch (error) {
+      return res.status(500).json({ ok: false, message: error });
+    }
+  }
+
   if (req.method === "DELETE") {
     try {
       const { query } = req;
