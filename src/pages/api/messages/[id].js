b3465beb0bcb035eee3e7db5ac154918e302519e
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-09-15 13:55:34 +0700

    fix api update mesage

--- src/pages/api/messages/[id].js
+++ src/pages/api/messages/[id].js
@@ -3,18 +3,10 @@ import { withSessionRoute } from "@/lib/sessions/withSession";
 
 async function handler(req, res) {
   const { user: userSession } = req.session;
-  console.log("kkkkk");
   if (req.method === "PATCH") {
     try {
-      const { id, id_receiver } = req.query;
-      console.log("kkkkkkkk", id_receiver);
-      const { body } = req;
-      const response = await updateMessages(
-        id,
-        body,
-        userSession.token,
-        id_receiver
-      );
+      const { query } = req;
+      const response = await updateMessages(userSession.token, query);
       return res.json(response);
     } catch (error) {
       console.log(error);
